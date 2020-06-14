8214d59fe42368c3384e692088f172cfc9423245
StevenH <sjhartman42@gmail.com>
2020-06-14 11:02:46 -0700

    removed mean functions 2nd try

--- cachematrix.R
+++ cachematrix.R
@@ -2,43 +2,6 @@
 ## a matrix, or retrieves the inverted matrix if it 
 ## is already cached
 
-# Mean test
-
-makeVector <- function(x = numeric()) {
-    m <- NULL
-    set <- function(y) {
-        x <<- y
-        m <<- NULL
-    }
-    get <- function() x
-    setmean <- function(mean) m <<- mean
-    getmean <- function() m
-    list(set = set, get = get,
-         setmean = setmean,
-         getmean = getmean)
-    }
-
-myVector <- makeVector(1:15)
-myVector
-m
-
-cachemean <- function(x, ...) {
-    m <- x$getmean()
-    if(!is.null(m)) {
-        message("getting cached data")
-        return(m)
-    }
-    data <- x$get()
-    m <- mean(data, ...)
-    x$setmean(m)
-    m
-}
-
-aVector <- makeVector()
-aVector$set(30:50)
-cachemean(aVector)
-aVector$getmean()
-
 ## makeCachMatrix generates a cache of a new matrix
 
 makeCacheMatrix <- function(x = matrix()) {
